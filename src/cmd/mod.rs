315cc53c444c8addc8ececc76ddb02351dcdf155
Saksham932007 <sakshamkapoor810@gmail.com>
2025-12-02 22:10:56 +0530

    feat: implement SET command and wire to database
    - Add Set variant to Command enum with key and value
    - Parse SET key value from command array
    - Validate argument count and types
    - Call db.write_entry() to persist data
    - Return Simple String OK on success
    - Extract key as String and value as Bytes

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -9,6 +9,9 @@ pub enum Command {
     /// PING [message] - Test connection
     Ping(Option<Bytes>),
     
+    /// SET key value - Set a key-value pair
+    Set { key: String, value: Bytes },
+    
     /// Unknown command
     Unknown(String),
 }
@@ -54,12 +57,36 @@ impl Command {
                     Err("ERR wrong number of arguments for 'ping' command".to_string())
                 }
             }
+            "SET" => {
+                // SET key value
+                if array.len() < 3 {
+                    return Err("ERR wrong number of arguments for 'set' command".to_string());
+                }
+                
+                let key = match &array[1] {
+                    Frame::Bulk(data) => {
+                        std::str::from_utf8(data)
+                            .map_err(|_| "invalid UTF-8 in key")?
+                            .to_string()
+                    }
+                    Frame::Simple(s) => s.clone(),
+                    _ => return Err("SET key must be a string".to_string()),
+                };
+                
+                let value = match array.remove(2) {
+                    Frame::Bulk(data) => data,
+                    Frame::Simple(s) => Bytes::from(s),
+                    _ => return Err("SET value must be a string".to_string()),
+                };
+                
+                Ok(Command::Set { key, value })
+            }
             _ => Ok(Command::Unknown(cmd_name)),
         }
     }
     
     /// Execute the command and write the response to the connection
-    pub async fn execute(&self, _db: &Db, dst: &mut Connection) -> Result<(), io::Error> {
+    pub async fn execute(&self, db: &Db, dst: &mut Connection) -> Result<(), io::Error> {
         match self {
             Command::Ping(msg) => {
                 let response = if let Some(msg) = msg {
@@ -67,6 +94,14 @@ impl Command {
                 } else {
                     Frame::Simple("PONG".to_string())
                 };
+                dst.write_frame(&response).await?;
+            }
+            Command::Set { key, value } => {
+                // Write to database
+                db.write_entry(key.clone(), value.clone());
+                
+                // Return OK
+                let response = Frame::Simple("OK".to_string());
                 dst.write_frame(&response).await?;
             }
             Command::Unknown(cmd) => {
